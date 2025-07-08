2c7f6c6983cdb67a049b1f3da43e51c9c3a66fc2
Talat El Beick <talatelbeick@gmail.com>
2025-07-09 01:48:17 +0300

    :sparkles: add google map location within address

---
+++ forza/forza/doctype/address_location/address_location.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Talat El Beick and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AddressLocation(Document):
+	pass
